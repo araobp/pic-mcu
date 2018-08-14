9cddc0ffa1a53352d88ab7ef0e562440a42f0653
arao <araobp@users.noreply.github.com>
2018-08-15 04:31:53 +0900

    update

---
+++ src/pic16f18326/speed_sensor.X/main.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.h
+ *
+ * Created on 2018/08/14, 16:27
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+extern uint8_t sum = 0;
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
